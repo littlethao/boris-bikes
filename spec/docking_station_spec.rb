f96939f97ba18eedd71281aa2dd6e61cf8bae79f
Thao Vo <ttthao.vo@gmail.com>
2016-10-23 19:55:57 +0100

    13th challenge completed: DockingStation to raise Error when docking a bike to a taken docking place

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,12 +5,6 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:bike) }
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
-
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
@@ -20,6 +14,13 @@ describe DockingStation do
 
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe '#dock' do
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 end
